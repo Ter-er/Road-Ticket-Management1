04889fd5ffbf156db8548cd1367163685ec52f08
Ter-er <tfgbem@gmail.com>
2024-12-24 02:46:23 +0100

    Authentication modifications for admin and official

--- User/urls.py
+++ User/urls.py
@@ -8,11 +8,14 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('admin_login/', views.admin_login_redirect, name='admin_login_redirect'),
+    path('admin_site/', views.admin_login_redirect, name='admin_login_redirect'),
     path('motorist_signup/', views.motorist_signup, name='motorist_signup'),
-    path('login_user/', views.login_user, name='login_user'),
+    # path('login_user/', views.login_user, name='login_user'),
     path('official_login/', views.official_login, name='official_login'),
+    path('admin_login/', views.admin_login, name='admin_login'),
+    path('dashboard_official/', views.dashboard_official, name='dashboard_official'),
+    path('dashboard_admin/', views.dashboard_admin, name='dashboard_admin'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('logout', views.logout, name='logout'),
+    path('logout_user', views.logout_user, name='logout_user'),
     #path('accounts/motorist_login/', auth_views.LoginView.as_view(), name='motorist_login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- User/views.py
+++ User/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from .models import Motorist 
 from .forms import MotoristSignupForm, LoginForm
+from django.contrib import messages # For Admin error message
+from django.utils.safestring import mark_safe # For Admin error message
+from django.urls import reverse  # For Admin error message
 
 # Create your views here.
 def home(request):
@@ -25,23 +28,23 @@ def motorist_signup(request):
 # def motorist_login(request):
 #     return render(request, 'motorist_login.html')
 
-def login_user(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate (request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('dashboard')  # Redirect to the home page or dashboard
-            else:
-                form.add_error(None, "Invalid username or password.")
-        else:
-            print(form.errors)  # For debugging purposes
-    else:
-        form = LoginForm()
-    return render(request, 'motorist_login.html', {'form': form})
+# def login_user(request):
+#     if request.method == 'POST':
+#         form = LoginForm(request.POST)
+#         if form.is_valid():
+#             username = form.cleaned_data['username']
+#             password = form.cleaned_data['password']
+#             user = authenticate (request, username=username, password=password)
+#             if user is not None:
+#                 login(request, user)
+#                 return redirect('dashboard')  # Redirect to the home page or dashboard
+#             else:
+#                 form.add_error(None, "Invalid username or password.")
+#         else:
+#             print(form.errors)  # For debugging purposes
+#     else:
+#         form = LoginForm()
+#     return render(request, 'motorist_login.html', {'form': form})
 
 
 
@@ -54,7 +57,7 @@ def official_login(request):
             user = authenticate (request, username=username, password=password)
             if user is not None:
                 login(request, user)
-                return redirect('dashboard')  # Redirect to the home page or dashboard
+                return redirect('dashboard_official')  # Redirect to the home page or dashboard
             else:
                 form.add_error(None, "Invalid username or password.")
         else:
@@ -62,12 +65,64 @@ def official_login(request):
     else:
         form = LoginForm()
     return render(request, 'official_login.html', {'form': form})
+
+
+
+def admin_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate (request, username=username, password=password)
+            if user is not None:
+                if user.is_staff:  # Check if the user is an admin
+                    login(request, user)
+                    return redirect('dashboard_admin')  # Redirect to the admin dashboard
+                else:
+
+                    official_login_url = reverse('official_login')
+                    # If the user is not an admin, show an error message and redirect to official login
+                    message = mark_safe(
+                        f'You are not an admin. Please log in as an official. <a href="{official_login_url}">Click here</a> to go to the official login page.'
+                    )
+                    messages.error(request, message)
+                    # return redirect('official_login')  # Redirect to the official login page
+            else:
+                form.add_error(None, "Invalid username or password.")
+        else:
+            print(form.errors)  # For debugging purposes
+    else:
+        form = LoginForm()
+
+    return render(request, 'admin_login.html', {'form': form})
+                
+                
+                
+                
+                
+    #             login(request, user)
+    #             return redirect('dashboard_admin')  # Redirect to the home page or dashboard
+    #         else:
+    #             form.add_error(None, "Invalid username or password.")
+    #     else:
+    #         print(form.errors)  # For debugging purposes
+    # else:
+    #     form = LoginForm()
+    # return render(request, 'admin_login.html', {'form': form})
         
 
 def dashboard(request):
     return render(request, 'dashboard_motorist.html')
 
-def logout(request):
+def dashboard_official(request):
+    return render(request, 'dashboard_official.html')
+
+def dashboard_admin(request):
+    return render(request, 'dashboard_admin.html')
+
+def logout_user(request):
+    logout(request)
     return redirect('home')
 
 def admin_login_redirect(request):
